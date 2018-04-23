6c94b528b013f8a056f3d8cfbfa171d2f05e4e0a
wangxuefeng18845893757 <1517926832@qq.com>
2018-04-23 22:50:51 +0800

    我的信息

--- client/pages/myInfo/myInfo.js
+++ client/pages/myInfo/myInfo.js
@@ -12,7 +12,14 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    // wx.request({
+    //   url: 'https://6kxrdzrv.qcloud.la/user/select_user',
+    //   //dataType:'json',
+    //   responseType:'text',
+    //   success:function(res){
+    //     console.log(res);
+    //   }
+    // })
   },
 
   /**
@@ -62,5 +69,53 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  to_attention:function(){
+    wx.navigateTo({
+      url: '../attention/attention',
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+  },
+  to_fans: function () {
+    wx.navigateTo({
+      url: '../fans/fans',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  to_help: function () {
+    wx.navigateTo({
+      url: '../help/help',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  to_aboutus: function () {
+    wx.navigateTo({
+      url: '../aboutus/aboutus',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  to_articles: function () {
+    wx.navigateTo({
+      url: '../articles/articles',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  to_notes: function () {
+    wx.navigateTo({
+      url: '../notes/notes',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
   }
 })
