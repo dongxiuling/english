f4d78dc9eef5dedb360b602925b90cf2d2cbad04
jipenfei <921374010@qq.com>
2018-05-01 12:27:42 +0800

    做笔记

---
+++ server/application/controllers/Note.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+use QCloud_WeApp_SDK\Mysql\Mysql as DB;
+class Note extends CI_Controller {
+
+
+  public function add_note(){
+     $content = $this->input->get("content");
+     $user_id = $this->input->get("user_id");
+     $this->load->model('note_model');
+		 $notes = $this->note_model->do_note($content,$user_id);
+		  echo json_encode($notes);
+    }
+}
